6ef1006708003f04d93fd09951fe97a10c7605a6
creedowl <creedowl@gmail.com>
2018-11-04 10:54:05 +0800

    add flask-migrate

--- nuaa_bot/bot/bot.py
+++ nuaa_bot/bot/bot.py
@@ -1,6 +1,17 @@
 from nuaa_bot.app import robot
+from nuaa_bot.models.user import User
 
 
 @robot.handler
 def hello(message):
     return 'Hello World!'
+
+
+@robot.key_click('auth')
+def auth(message, session):
+    open_id = message.source
+    query = User.query.filter_by(open_id=open_id)
+    if query.count() != 0:
+        return '已经认证'
+    user = query[0]
+    return 'auth'
